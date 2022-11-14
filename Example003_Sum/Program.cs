0a27131b226d2fbf6ea5809d6cc529f451c89180
idoctorvic <loukianova@yahoo.com>
2022-11-14 17:49:02 +0300

    add Example 003 Sum

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,6 @@
+﻿int number1 = 3;
+int number2 = 5;
+// Console.WriteLine(number1 + number2);
+// the result can ba saved in a variable and only then printed
+int result = number1 + number2;
+Console.WriteLine(result);
