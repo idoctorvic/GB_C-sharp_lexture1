9da3e2ba23b67d681a641915702dd743c87df7b0
idoctorvic <loukianova@yahoo.com>
2022-11-14 18:18:29 +0300

    add Random function

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,6 +1,13 @@
-﻿int number1 = 3;
-int number2 = 5;
+﻿// int number1 = 3;
+// int number2 = 5;
 // Console.WriteLine(number1 + number2);
 // the result can ba saved in a variable and only then printed
+// int result = number1 + number2;
+// Console.WriteLine(result);
+
+int number1 = new Random().Next(1, 10);
+Console.WriteLine(number1);
+int number2 = new Random().Next(1, 10);
+Console.WriteLine(number2);
 int result = number1 + number2;
 Console.WriteLine(result);
